ea5f942f9674db47999896f19c9c5cec800eada9
Daniel Kurz <danielkurz03@protonmail.com>
2024-03-20 16:17:33 +0100

    rm build

--- services/worker/build/dev/erlang/knet_gleam_ts/_gleam_artefacts/knet_gleam_ts.erl
+++
@@ -1,8 +0,0 @@
--module(knet_gleam_ts).
--compile([no_auto_import, nowarn_unused_vars, nowarn_unused_function, nowarn_nomatch]).
-
--export([main/0]).
-
--spec main() -> nil.
-main() ->
-    gleam@io:println(<<"Hello from knet_gleam_ts!"/utf8>>).
--- services/worker/build/dev/erlang/knet_gleam_ts/ebin/knet_gleam_ts.app
+++
@@ -1,8 +0,0 @@
-{application, knet_gleam_ts, [
-    {vsn, "1.0.0"},
-    {applications, [gleam_stdlib,
-                    gleeunit]},
-    {description, ""},
-    {modules, []},
-    {registered, []}
-]}.
